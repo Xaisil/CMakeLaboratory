56c0376ae3adf3b1c2195e8efd8a29c9c5d68ae3
Bartek Gowin <goblinek.bartek@gmail.com>
2021-03-12 14:05:09 +0100

    Added control over compilation process, here are used basic libraries for trygonometric functions calculations

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
-#include "trygonometria.h"
 #include <iostream>
+#include <LaborkaConfig.h>
 
+#ifdef USE_TRIGONOMETRY_DEGREE
+	#include "trygonometria.h"
+#else
+	#include <cmath>
+#endif
 int main() 
 { 
 	/*...*/ 
@@ -8,5 +13,10 @@ int main()
 	//std::cout << degreemath::cos(50.0);
 	//std::cout << degreemath::tg(50.0);
 	//std::cout << degreemath::ctg(50.0);
+	#ifdef USE_TRIGONOMETRY_DEGREE
+		result = degreematg::cos(45.0);
+	#else
+		result = cos(pi / 4.0);
+	#endif
 	return 0; 
 }
