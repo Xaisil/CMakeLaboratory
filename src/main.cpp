1ef410a4a40b72f7851fb8af8836168be616fecf
Bartek Gowin <goblinek.bartek@gmail.com>
2021-03-12 14:44:46 +0100

    Added 4 tests for library, and check if tests should be built

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 #include <LaborkaConfig.h>
+#include <cstdlib>
 
 #ifdef USE_TRIGONOMETRY_DEGREE
 	#include "trygonometria.h"
 #else
 	#include <cmath>
 #endif
-int main() 
+
+int main(int argc, char* argv[])
 { 
 	/*...*/ 
-	//std::cout << degreemath::sin(50.0);
-	//std::cout << degreemath::cos(50.0);
-	//std::cout << degreemath::tg(50.0);
-	//std::cout << degreemath::ctg(50.0);
+
+	double result;
+	int input = 0;
+	if (argc > 1)
+	{
+		input = atoi(argv[1]);
+	}
 	#ifdef USE_TRIGONOMETRY_DEGREE
-		result = degreematg::cos(45.0);
+		result = degreemath::sin(input);
 	#else
-		result = cos(pi / 4.0);
+		result = sin(pi / 4.0);
 	#endif
+	
+	std::cout << result<<std::endl;
 	return 0; 
 }
